936cf0791e447aa3c573a2af168e2ca9c5587baa
Daniel Dias Costa <Daniel Dias@prodespluz.local>
2018-08-13 17:33:54 -0300

    UI Components with Angular Bootstrap.

---
+++ Mod3Lab/app/labController.js
@@ -0,0 +1,23 @@
+angular.module('app')
+.controller('labController', [
+    function() {
+
+        var vm = this;
+        
+        vm.alerts = [];
+
+        vm.addDanger = function() {
+            addAlert('danger', 'Danger, Will Robinson! Danger!');
+        };
+        vm.addWarning = function() {
+            addAlert('warning', 'Warning! Warning! Alien approaching!')
+        };
+        vm.dismissAlert = function(index) {
+            vm.alerts.splice(index, 1);
+        };
+
+        function addAlert(type, text) {
+            vm.alerts.push({type: type, text: text});
+        }
+    }
+]);
